02be8a3274a6cf30bd484fe515070141c6f4c306
Raymart Madrigal <raymart.madrigal2411@gmail.com>
2022-11-06 23:16:59 +0800

    First Commit

---
+++ src/main/java/com/raymart/cwt/CSVCompiler/CsvCompilerApplication.java
@@ -0,0 +1,13 @@
+package com.raymart.cwt.CSVCompiler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CsvCompilerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CsvCompilerApplication.class, args);
+	}
+
+}
