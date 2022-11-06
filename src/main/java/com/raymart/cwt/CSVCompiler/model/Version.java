19318e7a6882cfd2a9f216b5e3b860b55516a325
Raymart <116375155+rmadrigalcwt@users.noreply.github.com>
2022-11-07 02:21:52 +0800

    Added model for version

---
+++ src/main/java/com/raymart/cwt/CSVCompiler/model/Version.java
@@ -0,0 +1,41 @@
+package com.raymart.cwt.CSVCompiler.model;
+
+public class Version {
+
+    private String hotelName;
+    private String hotelId;
+    private String harpId;
+    private String recordId;
+
+    public String getHotelName() {
+        return hotelName;
+    }
+
+    public void setHotelName(String hotelName) {
+        this.hotelName = hotelName;
+    }
+
+    public String getHotelId() {
+        return hotelId;
+    }
+
+    public void setHotelId(String hotelId) {
+        this.hotelId = hotelId;
+    }
+
+    public String getHarpId() {
+        return harpId;
+    }
+
+    public void setHarpId(String harpId) {
+        this.harpId = harpId;
+    }
+
+    public String getRecordId() {
+        return recordId;
+    }
+
+    public void setRecordId(String recordId) {
+        this.recordId = recordId;
+    }
+}
